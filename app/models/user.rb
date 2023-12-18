68b628f3b23f25f1c4eb46c1426e082f2c5bd828
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-12-18 21:49:57 +0430

    resolve linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence => true
   has_many :recipes, dependent: :destroy
   has_many :foods, dependent: :destroy
 end
