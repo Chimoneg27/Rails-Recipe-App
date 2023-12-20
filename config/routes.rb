88e9a4aa0c2d9a1b6066628f7e8526f74c4eafa1
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-12-20 20:37:13 +0430

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  resources :shopping_lists
   resources :foods, only: [:index, :show, :new, :create, :destroy]
+  resources :shopping_list, only: [:index]
   resources :recipes, only: [:index, :show, :new, :create, :destroy] do
-    resources :recipe_foods, only: [:new, :create]
+    resources :recipe_foods, only: [:new, :create, :edit, :update, :destroy]
       member do
               put :toggle_public
     end
   end
   resources :users, only: [:index, :show, :new, :create, :destroy]
-
   root to: 'foods#index'
   get '/public_recipes', to: 'recipes#public_recipes', as: 'public_recipes'
   # get "up" => "rails/health#show", as: :rails_health_check
