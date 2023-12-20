c88efb7c77b4bd612b72e4332c1a25c02aa6ceb7
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-12-20 21:00:15 +0430

    update shopping list view and controller

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -4,7 +4,6 @@ class ShoppingListController < ApplicationController
     @food = Food.includes(:recipe_foods).where(user_id: current_user.id)
     @total_price = 0
     displayed_names = []
-
     @shopping_list = []
 
     @recipe_foods.each do |recipe_food|
@@ -14,13 +13,15 @@ class ShoppingListController < ApplicationController
         unless displayed_names.include?(recipe_food.food.name)
           displayed_names << recipe_food.food.name
           @total_price += recipe_food.food.price * recipe_food.quantity
-
-          # Add the item to shopping_list
-          @shopping_list << { name: recipe_food.food.name, quantity: recipe_food.quantity }
+          # Add the item to shopping_list including price.
+          @shopping_list << { name: recipe_food.food.name, quantity: recipe_food.quantity,
+                              price: (recipe_food.food.price * recipe_food.quantity) }
         end
       end
     end
 
     @displayed_names = displayed_names
+    # Set total cost for access in view.
+    @total_cost = @total_price
   end
 end
