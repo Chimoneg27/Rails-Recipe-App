461635f15be84f241eae175a597f9b4f1cc6553a
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-12-21 14:50:54 +0430

    add test for food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,4 +3,6 @@ class Food < ApplicationRecord
   has_many :recipe_foods, dependent: :destroy
   validates :name, presence: true
   validates :measurement_unit, presence: true
+  validates :price, presence: true
+  validates :quantity, presence: true
 end
